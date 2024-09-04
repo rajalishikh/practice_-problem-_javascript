6a13336b22715bc3e483a97283bf10ea95c85b9c
unknown <nahidujjamanraj@gmail.com>
2024-09-04 21:41:53 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,7 @@
+var my_budget_for_shopping=1000
+var price_1kg_of_orange=200
+var price_1kg_of_apples=300
+
+var total_price_food=price_1kg_of_apples + price_1kg_of_orange;
+var my_at_least_money=my_budget_for_shopping-total_price_food;
+console.log('Now I back my money',my_at_least_money,"taka")
