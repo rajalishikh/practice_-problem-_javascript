375451fb448368f2bfeb944bb611063e64ee0055
unknown <nahidujjamanraj@gmail.com>
2024-09-04 21:52:32 +0600

    In this section  I solve a simple problem by using javascript

---
+++ 2nd.js
@@ -0,0 +1,11 @@
+var Bangla = 75.25;
+var Mathematics=65;
+var Biology =80;
+var chemistry= 35.45;
+var physics= 99.50; 
+
+var total_number=Bangla+Mathematics+Biology+chemistry+physics;
+
+var total_average=total_number/5;
+var result=total_average.toFixed(2)
+console.log(result)
