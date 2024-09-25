a468a77836781d27dc5dcf2122fc7e23b601f8a8
unknown <nahidujjamanraj@gmail.com>
2024-09-25 20:56:54 +0600

    I solve a simple problem about array

---
+++ array1.js
@@ -0,0 +1,15 @@
+// problem 
+// 1. Declare an array
+// Declare an array with 5 elements containing fruits
+// console log the 3rd index element
+// change the value of the 2nd index element to jambura
+// console log the final array
+
+// problem solving 1
+
+let fruits=['Apple', 'Strawberry','Pineapple','Watermelon','Mango']
+fruits[2]= 'jambura'
+console.log(fruits[3])
+
+console.log(fruits)
+
